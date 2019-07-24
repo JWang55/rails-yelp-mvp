b6c842b8913f86f591982469a3e5981792040675
Jinhong Wang <1320358947@qq.com>
2019-07-24 17:32:13 +0800

    new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index', to:'restaurants#index'
+  # get 'restaurants/:id', to:'restaurants#show'
+  # get 'restaurants/new', to:'restaurants#new'
+  # post 'restaurants/index', to:'restaurants#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only:[:index, :create, :new]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+# restaurant_attributes = []
+Restaurant.destroy_all
+puts "Seeding new life "
+
+10.times do
+  attributes = {
+    name: Faker::Restaurant.name,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number
+  }
+  # restaurant_attributes << attributes
+  Restaurant.create!(attributes)
+end
+
+puts Restaurant.all
+# Restaurant.create!(restaurant_attributes)
